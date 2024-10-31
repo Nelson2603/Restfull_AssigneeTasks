4a460505425dd0f1a6d58999b56c72a94512befe
Nelson2603 <157066286+Nelson2603@users.noreply.github.com>
2024-10-31 23:41:39 +0400

    3.1 added interfaces.

--- src/main/java/org/example/restfull_assigneetasks/repository/AssigneeRepository.java
+++ src/main/java/org/example/restfull_assigneetasks/repository/AssigneeRepository.java
@@ -2,6 +2,6 @@ package org.example.restfull_assigneetasks.repository;
 
 import org.example.restfull_assigneetasks.model.entity.Assignee;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+//создали репозиторий
 public interface AssigneeRepository extends JpaRepository<Assignee, Long> {
 }
