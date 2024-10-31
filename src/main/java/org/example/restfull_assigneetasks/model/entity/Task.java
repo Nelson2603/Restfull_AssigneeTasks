83219b9c9e799965d2a28c6a1267b96c2d4ad3d0
Nelson2603 <157066286+Nelson2603@users.noreply.github.com>
2024-11-01 00:00:50 +0400

    write commit

--- src/main/java/org/example/restfull_assigneetasks/model/entity/Task.java
+++ src/main/java/org/example/restfull_assigneetasks/model/entity/Task.java
@@ -10,7 +10,7 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class Task {
-    @Id
+    @Id//здесь был Наиль
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
     private String name;
